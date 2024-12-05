c71a41a0079f4de70adbc67852d522b56e096f6d
tran-daniel4 <tdanielhuynh@gmail.com>
2024-12-04 23:29:12 -0500

    Developing project structure

---
+++ src/main/java/com/example/ems_backend/mapper/EmployeeMapper.java
@@ -0,0 +1,24 @@
+package com.example.ems_backend.mapper;
+
+import com.example.ems_backend.dto.EmployeeDto;
+import com.example.ems_backend.entity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static EmployeeDto mapToEmployee(EmployeeDto employeeDto) {
+        return new EmployeeDto(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
---
+++ src/main/java/com/example/ems_backend/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.example.ems_backend.repository;
+
+import com.example.ems_backend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
