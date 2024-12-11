9b969aa5984eecc920f6ebf2a4fc799a80ed7fea
tran-daniel4 <tdanielhuynh@gmail.com>
2024-12-11 00:47:55 -0500

    Adding employeecomponent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,22 @@ import './App.css'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import EmployeeComponent from './components/EmployeeComponent'
 
 function App() {
 
   return (
     <>
-      <HeaderComponent/>
-      <ListEmployeeComponent/>
-      <FooterComponent/>
+      <BrowserRouter>
+        <HeaderComponent/>
+        <Routes>
+          <Route path='/' element={ <ListEmployeeComponent/> }></Route>
+          <Route path='/employees' element={ <ListEmployeeComponent/> }></Route>
+          <Route path='/add-employee' element={ <EmployeeComponent/> }></Route>
+        </Routes>
+        <FooterComponent/>
+      </BrowserRouter>
     </>
   )
 }
